7c7f1756cb700bb5b7ecd7a725b93968efc561f3
JasonLuis <jason20luis@gmail.com>
2026-05-18 11:13:35 -0300

    feat: atualizar rotas para incluir proteção de acesso e redirecionamento

--- frontend/fintrack/src/app/routes/index.tsx
+++ frontend/fintrack/src/app/routes/index.tsx
@@ -1,14 +1,36 @@
 import Login from '@/features/auth/pages/Login'
 import Dashboard from '@/features/dashboard/Dashborad'
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, Navigate } from 'react-router-dom'
+import { GuestRoute } from './guards/GuestRoute'
+import { ProtectedRoute } from './guards/ProtectedRoute'
+import { Layout } from '@/components/layout'
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <Login />
+    element: <Navigate to="/login" replace />
   },
   {
-    path: '/dashboard',
-    element: <Dashboard />
+    element: <GuestRoute />, //verifica se o usuario esta logado e caso esteja, envia para a tela de dashboard
+    children: [
+      {
+        path: '/login',
+        element: <Login />
+      }
+    ]
+  },
+  {
+    element: <ProtectedRoute />, // proteção de rotas, caso não esteja logado, envia para a tela de login
+    children: [
+      {
+        element: <Layout />, // adiciona layout a pagina
+        children: [
+          {
+            path: '/dashboard',
+            element: <Dashboard />
+          }
+        ]
+      }
+    ]
   }
 ])
